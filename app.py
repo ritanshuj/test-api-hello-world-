9783775994516e80c595a57697c5e13285c041b4
ritanshu jayant <67803038+ritanshuj@users.noreply.github.com>
2021-04-07 20:56:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')
+    return "hello world"
 
 if __name__=="__main__":
-    app.run(debug=True,use_reloader=False) 
+    app.run() 
